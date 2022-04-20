5f036fe788cdd8b3700ee067bb6967afd00e2d62
shahrokh <shahrokhelmi@gmail.com>
2022-04-20 20:14:59 +0430

    l6 - end - drag and drop

--- src/store.js
+++ src/store.js
@@ -23,13 +23,22 @@ export default new Vuex.Store({
     UPDATE_TASK (state, { task, key, value }) {
       task[key] = value
     },
-    MOVE_TASK (state, { tasks, indexColumn, indexTask }) {
-      // console.log('from column: ',indexColumn)
-      // console.log('from task: ',indexTask)
-      // console.log('which task to move:',state.board.columns[indexColumn].tasks)
-      // console.log('where to move tasks: ', tasks)
-      const task = state.board.columns[indexColumn].tasks.splice(indexTask, 1)[0]
-      tasks.push(task)
+    // MOVE_TASK (state, { tasks, indexColumn, indexTask }) {
+    //   console.log('from column: ', indexColumn)
+    //   console.log('from task: ', indexTask)
+    //   // console.log('which task to move:',state.board.columns[indexColumn].tasks)
+    //   // console.log('where to move tasks: ', tasks)
+    //   const task = state.board.columns[indexColumn].tasks.splice(indexTask, 1)[0]
+    //   tasks.push(task)
+    // },
+    MOVE_TASK (state, { indexFromColumn, indexFromTask, toTaskIndex, toColumnIndex }) {
+      console.log(indexFromColumn, indexFromTask, toTaskIndex, toColumnIndex)
+      const task = state.board.columns[indexFromColumn].tasks.splice(indexFromTask, 1)[0]
+      state.board.columns[toColumnIndex].tasks.splice(toTaskIndex, 0, task)
+    },
+    MOVE_COLUMN (state, { fromColumnIndex, toColumnIndex }) {
+      const column = state.board.columns.splice(fromColumnIndex, 1)[0]
+      state.board.columns.splice(toColumnIndex, 0, column)
     }
   },
   getters: {
--- src/views/Board.vue
+++ src/views/Board.vue
@@ -2,24 +2,30 @@
   <div class="board">
     <div class="flex flex-row items-start">
       <div
-        @drop="dropTask($event, column.tasks)"
+        draggable
+        @dragstart="pickColumn($event,$columnIndex)"
+        @drop="dropTaskOrColumn($event, column.tasks, $columnIndex)"
         @dragover.prevent
         @dragenter.prevent
         class="column"
         v-for="(column, $columnIndex) in board.columns"
         :key="$columnIndex">
         <div class="flex items-center mb-2 font-bold">
-          {{ column.name }}
+          {{ column.name }} -- {{ $columnIndex }}
         </div>
         <div class="list-reset">
           <div
             draggable
             @dragstart="pickupTask($event, $taskIndex, $columnIndex)"
+            @drop="dropTask($event,$taskIndex, $columnIndex)"
+            @dragover.prevent
+            @dragenter.prevent
+
             class="task"
             v-for="(task, $taskIndex) in column.tasks"
             :key="$taskIndex"
             @click="openTask(task)">
-            <span class="w-full flex-no-shrink font-bold">{{ task.name }} {{ $taskIndex }} {{ $columnIndex }}</span>
+            <span class="w-full flex-no-shrink font-bold">{{ task.name }} {{ $taskIndex }} -- {{ $columnIndex }}</span>
             <p v-if="task.description" class="w-full flex-no-shrink mt-1 text-sm">{{ task.description }}</p>
           </div>
           <input
@@ -59,14 +65,43 @@ export default {
       e.target.value = ''
     },
     pickupTask (e, $taskIndex, $columnIndex) {
-      e.dataTransfer.setData('task-index', $taskIndex)
-      e.dataTransfer.setData('column-index', $columnIndex)
-    },
-    dropTask (e, tasks) {
-      const indexColumn = e.dataTransfer.getData('column-index')
-      const indexTask = e.dataTransfer.getData('task-index')
+      e.dataTransfer.effectAllowed = 'move'
+      e.dataTransfer.dropEffect = 'move'
 
-      this.$store.commit('MOVE_TASK', { tasks, indexColumn, indexTask })
+      if ($taskIndex !== undefined) {
+        e.dataTransfer.setData('task-index', $taskIndex)
+        e.dataTransfer.setData('column-index', $columnIndex)
+        e.dataTransfer.setData('type', 'task')
+      }
+    },
+    dropTaskOrColumn (e, tasks, toColumnIndex) {
+      // if (e.dataTransfer.getData('type') === 'task') {
+      //   console.log('dropTask event: ', e)
+      //   const indexColumn = e.dataTransfer.getData('column-index')
+      //   const indexTask = e.dataTransfer.getData('task-index')
+      //   this.$store.commit('MOVE_TASK', { tasks, indexColumn, indexTask })
+      // } else {
+      //   console.log('dropColumn event: ', e)
+      //   const fromColumnIndex = e.dataTransfer.getData('column-index')
+      //   this.$store.commit('MOVE_COLUMN', { fromColumnIndex, toColumnIndex })
+      // }
+
+      if (e.dataTransfer.getData('type') !== 'task') {
+        console.log('dropColumn event: ', e)
+        const fromColumnIndex = e.dataTransfer.getData('column-index')
+        this.$store.commit('MOVE_COLUMN', { fromColumnIndex, toColumnIndex })
+      }
+    },
+    pickColumn (e, columnIndex) {
+      e.dataTransfer.setData('column-index', columnIndex)
+    },
+    dropTask (e, toTaskIndex, toColumnIndex) {
+      if (e.dataTransfer.getData('type') === 'task') {
+        console.log('drop taskkkkkl:', e)
+        const indexFromColumn = e.dataTransfer.getData('column-index')
+        const indexFromTask = e.dataTransfer.getData('task-index')
+        this.$store.commit('MOVE_TASK', { indexFromColumn, indexFromTask, toTaskIndex, toColumnIndex })
+      }
     }
   }
 
